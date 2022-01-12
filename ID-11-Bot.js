d6b56abef672b17ea755441666ad7a9f51e23cec
SWRP-GitHub <96972616+SWRP-GitHub@users.noreply.github.com>
2022-01-11 18:02:50 -0600

    adding new verified files
    Bot Still works and has not imploded yet.
    committing my changes now before i loose power or some shit

--- ID-11-Bot.js
+++ ID-11-Bot.js
@@ -3,12 +3,13 @@ const Discord = require("discord.js");
 const sf = require('./sharedFunctions.js');
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_INTEGRATIONS"]});
 const config = require("./config/config.json");
+const ruleFile = "placeholder.txt"
 const discordConfig = config.discordCreds;
 
 client.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
   console.log(`The bot is online.`); 
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  client.user.setActivity(`Doin your mom, yea we strait doin your mom`);
 });
 
 client.on("guildCreate", guild => {
@@ -39,7 +40,7 @@ client.on("messageCreate", async message => {
 
   ///lastpost embed
   if(command === "lastpost") {
-    let postID = await sf.getHotPosts();
+    let postID = await sf.getHotPosts("content");
     let postTitle = await sf.getPostData("Title", postID[2]);
     let postBody = await sf.getPostData("Body",postID[2]);
     let postURL = await sf.getPostData("URL", postID[2]);
@@ -50,14 +51,17 @@ client.on("messageCreate", async message => {
      .setURL(postURL)
      .setAuthor(postAuthor.name, postIcon)
      .setTitle(postTitle)
-     .setDescription(postBody.slice(0,4096));
+     .setDescription(postBody.slice(0,1000)+'.....\n   |CONTINUE READING ON REDDIT|')
+     .setFooter(`[IG-88-Bot]: Brought to you by Malgus`)
+     .setTimestamp()
+     .setImage("https://styles.redditmedia.com/t5_2tg2b/styles/bannerBackgroundImage_065dobtwx9b71.png")
      message.channel.send({ embeds: [embed] });
   };
 
 
   ///Story and Setting embed
   if(command === "setting") {
-    let postID = await sf.getHotPosts();
+    let postID = await sf.getHotPosts("content");
     let postTitle = await sf.getPostData("Title", postID[0]);
     let postBody = await sf.getPostData("Body",postID[0]);
     let postURL = await sf.getPostData("URL", postID[0]);
@@ -68,26 +72,72 @@ client.on("messageCreate", async message => {
      .setURL(postURL)
      .setAuthor(postAuthor.name, postIcon)
      .setTitle(postTitle)
-     .setDescription(postBody.slice(0,4096));
+     .setDescription(postBody.slice(0,1000)+'.....\n   |CONTINUE READING ON REDDIT|')
+     .setFooter(`[IG-88-Bot]: Brought to you by Malgus`)
+     .setTimestamp()
+     .setImage("https://styles.redditmedia.com/t5_2tg2b/styles/bannerBackgroundImage_065dobtwx9b71.png")
      message.channel.send({ embeds: [embed] });
   };
 
   /////episode embed
   if(command === "episode") {
-    let postID = await sf.getHotPosts();
+    let postID = await sf.getHotPosts("content");
     let postTitle = await sf.getPostData("Title", postID[1]);
     let postBody = await sf.getPostData("Body",postID[1]);
     let postURL = await sf.getPostData("URL", postID[1]);
     let postAuthor = await sf.getPostData("Author", postID[1]);
     let postIcon = await sf.getPostData("AuthorIcon", postID[1]);
     const embed = new Discord.MessageEmbed()
-     .setColor(0xF1C232)
+     .setColor(0x8f39c4)
+     .setImage("https://styles.redditmedia.com/t5_2tg2b/styles/bannerBackgroundImage_065dobtwx9b71.png")
      .setURL(postURL)
      .setAuthor(postAuthor.name, postIcon)
      .setTitle(postTitle)
-     .setDescription(postBody.slice(0,4096))
+     .setDescription(postBody.slice(0,1000)+'.....\n  |CONTINUE READING ON REDDIT|')
+     .setFooter(`[IG-88-Bot]: Brought to you by Malgus`)
+     .setTimestamp()
+     
      message.channel.send({ embeds: [embed] });
   };
+  //New testing
+  if(command === "getnewpostlist") {
+    let postTitle = await sf.getNewPosts('title');
+    let postURL = await sf.getNewPosts('url');
+    message.channel.send('**Generating New Post List:** ')
+      for(i in postTitle){
+        message.channel.send(`**${i}:** ` + postTitle[i] + '\n' + postURL[i])
+      }
+    }
+  
+  if(command === "getnewposts") {
+    let titles = await sf.getNewPostsV2('Starwarsrp','title');
+    let authors = await sf.getNewPostsV2('Starwarsrp','author');
+    let postIDs = await sf.getNewPostsV2('Starwarsrp','postid');
+    let postURLs = await sf.getNewPostsV2('Starwarsrp','url');
+    let postIcons = await sf.getNewPostsV2('Starwarsrp','authicon');
+    message.channel.send('**New Posts:** \n===================== ')
+    for(i in titles){
+      let postBodys = await sf.getPostData("Body",postIDs[i]);
+      //console.log(postBodys)
+     // let postURLs = await sf.getPostData("URL", postIDs[i]);
+      //console.log(postURLs)
+     // let authors = await sf.getPostData("Author", postIDs[i]);
+     // let postIcons = await sf.getPostData("AuthorIcon", postIDs[i]);
+      const embed = new Discord.MessageEmbed()
+      .setColor(0x3F5061)
+      .setImage("https://styles.redditmedia.com/t5_2tg2b/styles/bannerBackgroundImage_065dobtwx9b71.png")
+      .setURL(postURLs[i])
+      .setAuthor(authors[i],postIcons[i])
+      .setTitle(titles[i])
+      .setDescription(postBodys.slice(0,1000) + ".....\n |CONTINUE READING ON REDDIT|")
+      .setFooter(`[IG-88-Bot]: Brought to you by Malgus`)
+      .setTimestamp()
+      message.channel.send({ embeds: [embed] });
+     
+     
+      //message.channel.send(`**${i}:** ` + titles[i] + "\n" + '**Author**: ' + authors[i] + '\n'+`**PostID**: ${postIDs[i]} ` +`\n **PostURL: ${postURLs[i]}**`)
+    }
+  }
 });
 
 client.login(discordConfig.token);
--- sharedFunctions.js
+++ sharedFunctions.js
@@ -2,6 +2,9 @@ const { RedditContent } = require('snoowrap');
 const snoowrap = require('snoowrap'); //Reddit lib
 const config = require('./config/config.json'); //Config lib
 const defaultConfig = config.creds; //Config
+const fs = require('fs');
+const command = require('nodemon/lib/config/command');
+const { title } = require('process');
 
 //SnooWrap request with OAuth credentials
 const r = new snoowrap({
@@ -14,12 +17,21 @@ const r = new snoowrap({
  //////////////////////////////////FUNCTIONS/////////////////////////////////////////
 
  //Get Hot Posts from SWRP
-  async function getHotPosts(){
-    let promise = new Promise((resolve,reject)=>{
-        setTimeout(()=>resolve(r.getHot('StarwarsRP').map(RedditContent => RedditContent.id)))
-    })
-    let result = await promise
-    return Promise.resolve(result)
+  async function getHotPosts(input){
+    if(input === "content"){
+        let promise = new Promise((resolve,reject)=>{
+            setTimeout(()=>resolve(r.getHot('StarwarsRP').map(RedditContent => RedditContent.id)))
+        })
+        let result = await promise
+        return Promise.resolve(result)
+    }
+    if(input === "title"){
+        let promise = new Promise((resolve,reject)=>{
+            setTimeout(()=>resolve(r.getHot('Starwarsrp').map(RedditContent => RedditContent.title)))
+        })
+        let result = await promise
+        return Promise.resolve(result)
+    }
   };
 
 //Get Post Data
@@ -29,22 +41,25 @@ const r = new snoowrap({
 async function getPostData(segment, postID){
     let promise = new Promise((resolve, reject) => {
         if(segment == 'Body'){
-            setTimeout(() => resolve(r.getSubmission(postID).selftext) , 1500)
+            setTimeout(() => resolve(r.getSubmission(postID).selftext) , 100)
         }
         if(segment == 'Title'){
-            setTimeout(() => resolve(r.getSubmission(postID).title) , 1500)
+            setTimeout(() => resolve(r.getSubmission(postID).title) , 100)
         }
         if(segment == 'postID'){
-            setTimeout(()=> resolve(r.getSubmission(postID).postID), 1500)
+            setTimeout(()=> resolve(r.getSubmission(postID).postID), 100)
         }
         if(segment == 'URL'){
-            setTimeout(()=> resolve(r.getSubmission(postID).url), 1500)
+            setTimeout(()=> resolve(r.getSubmission(postID).url), 100)
         }
         if(segment == "Author"){
-            setTimeout(()=> resolve(r.getSubmission(postID).author), 1500)
+            setTimeout(()=> resolve(r.getSubmission(postID).author), 100)
         }
         if(segment == "AuthorIcon"){
-            setTimeout(()=> resolve(r.getSubmission(postID).author.icon_img), 1500)
+            setTimeout(()=> resolve(r.getSubmission(postID).author.icon_img), 100)
+        }
+        if(segment == "PostTitle"){
+            setTimeout(()=> resolve(r.getSubmission(postID).title), 100)
         }
     });
     let result = await promise;
@@ -52,5 +67,57 @@ async function getPostData(segment, postID){
 };
 
 
-//Exports functions
-  module.exports = {getHotPosts,getPostData};
+////gets the new posts
+async function getNewPosts(type){
+    if(type === 'title'){
+        let promise = r.getNew('Starwarsrp').map(RedditContent => RedditContent.title)
+        let result = await promise;
+        return result
+    }
+    if(type === 'url'){
+        let promise = r.getNew('Starwarsrp').map(RedditContent => RedditContent.url)
+        let result = await promise;
+        return result
+    }
+}
+
+
+
+//////Get lists of things and content
+//////You can basically just construct a list of things here and then for loop it to map the data accordingly in a discord post.
+async function getNewPostsV2(subred,segment){
+    if(segment === 'title'){
+        let promisePostTitles = r.getNew(subred, {limit: 3}).map(RedditContent => RedditContent.title)
+        let result = await promisePostTitles;
+        return result
+    }
+    if(segment === 'author'){
+        let promisePostAuthors = r.getNew(subred,{limit: 3}).map(RedditContent => RedditContent.author.name)
+        let result = await promisePostAuthors;
+        return result
+    }
+    if(segment === 'postid'){
+        let promisePostID = r.getNew(subred,{limit: 3}).map(RedditContent => RedditContent.id)
+        let result = await promisePostID
+        return result 
+    }
+    if(segment === 'url'){
+        let promisePostURL = r.getNew(subred,{limit: 3}).map(RedditContent => RedditContent.url)
+        let result = await promisePostURL
+        return result
+    }
+    if(segment === 'authicon'){
+        let promiseAuthIcon = r.getNew(subred,{limit: 3}).map(RedditContent => RedditContent.author.icon_img)
+        let result = await promiseAuthIcon
+        return result
+    }
+    if(segment === 'body'){
+        let promiseAuthIcon = r.getNew(subred,{limit: 3}).map(RedditContent => RedditContent.body)
+        let result = await promiseAuthIcon
+        return result
+    }
+}
+
+
+
+  module.exports = {getHotPosts,getPostData,getNewPosts,getNewPostsV2};
